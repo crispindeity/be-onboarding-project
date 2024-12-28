5e5651cffdea1e8a37c17c537ab59e350b372521
crispin <h.c.shin.dev09@gmail.com>
2024-12-28 18:59:40 +0900

    :white_check_mark: Test: 설문조사 테스트 추가

--- survey-core/src/test/kotlin/study/crispin/surveycore/service/SurveyServiceTest.kt
+++ survey-core/src/test/kotlin/study/crispin/surveycore/service/SurveyServiceTest.kt
@@ -7,6 +7,7 @@ import io.kotest.matchers.shouldBe
 import study.crispin.surveycore.domain.Form
 import study.crispin.surveycore.domain.Survey
 import study.crispin.surveycore.domain.SurveyItem
+import study.crispin.surveycore.extension.toDomain
 import study.crispin.surveycore.fake.SubmitFakePort
 import study.crispin.surveycore.fake.SurveyFakePort
 
@@ -62,6 +63,142 @@ class SurveyServiceTest : DescribeSpec({
                         actual.name shouldBe "관심사 조사"
                         actual.items shouldHaveSize 3
                     }
+                }
+            }
+        }
+
+        describe("설문 조사 업데이트 테스트") {
+
+            context("정상적인 업데이트 요청일 때") {
+
+                it("설문조사를 업데이트 할 수 있어야 한다.") {
+                    // given
+                    val items: List<SurveyItem> = listOf(
+                        SurveyItem(
+                            "이름",
+                            "성함을 입력해주세요.",
+                            Form.ShortInput,
+                            true
+                        ),
+                        SurveyItem(
+                            "나이",
+                            "나이을 입력해주세요.",
+                            Form.ShortInput,
+                            false
+                        ),
+                        SurveyItem(
+                            "관심사",
+                            "관심 있는 분야를 모두 선택해주세요.",
+                            Form.MultiSelect(listOf("스포츠", "음약", "기술")),
+                            true
+                        )
+                    )
+                    val createRequest = CreateSurveyUseCase.Request(
+                        "관심사 조사",
+                        "나이에 따른 관심사 조사",
+                        items,
+                    )
+                    val createdSurvey: Survey = surveyService.createSurvey(createRequest)
+
+                    val request = UpdateSurveyUseCase.Request(
+                        createdSurvey.id!!,
+                        "연령별 관심사 조사",
+                        "연령에 따른 관심사 조사",
+                    )
+
+                    // when
+                    surveyService.updateSurvey(request)
+
+                    // then
+                    val actual: Survey = surveyService.findSurvey(
+                        FindSurveyUseCase.Request(createdSurvey.id!!)
+                    ).toDomain()
+
+                    assertSoftly {
+                        actual.name shouldBe "연령별 관심사 조사"
+                        actual.description shouldBe "연령에 따른 관심사 조사"
+                    }
+                }
+            }
+        }
+
+        describe("설문 조사 항목 업데이트 테스트") {
+
+            context("정상적인 업데이트 요청일 때") {
+
+                it("설문조사 항목을 업데이트 할 수 있어야 한다.") {
+                    // given
+                    val items: List<SurveyItem> = listOf(
+                        SurveyItem(
+                            "이름",
+                            "성함을 입력해주세요.",
+                            Form.ShortInput,
+                            true
+                        ),
+                        SurveyItem(
+                            "나이",
+                            "나이을 입력해주세요.",
+                            Form.ShortInput,
+                            false
+                        ),
+                        SurveyItem(
+                            "관심사",
+                            "관심 있는 분야를 모두 선택해주세요.",
+                            Form.MultiSelect(listOf("스포츠", "음약", "기술")),
+                            true
+                        )
+                    )
+                    val createRequest = CreateSurveyUseCase.Request(
+                        "관심사 조사",
+                        "나이에 따른 관심사 조사",
+                        items,
+                    )
+                    val createdSurvey: Survey = surveyService.createSurvey(createRequest)
+
+                    val updateItems: List<SurveyItem> = listOf(
+                        SurveyItem(
+                            "이름",
+                            "성함을 입력해주세요.",
+                            Form.ShortInput,
+                            true
+                        ),
+                        SurveyItem(
+                            "나이",
+                            "나이을 입력해주세요.",
+                            Form.ShortInput,
+                            false
+                        ),
+                        SurveyItem(
+                            "관심사",
+                            "관심 있는 분야를 모두 선택해주세요.",
+                            Form.MultiSelect(listOf("스포츠", "음약", "기술")),
+                            true
+                        ),
+                        SurveyItem(
+                            "성별",
+                            "성별을 선택해 주세요.",
+                            Form.SingleSelect(listOf("남", "여", "기타")),
+                            true
+                        ),
+                    )
+
+
+                    val request = UpdateSurveyUseCase.Request(
+                        createdSurvey.id!!,
+                        createdSurvey.name,
+                        createdSurvey.description,
+                        updateItems,
+                    )
+
+                    // when
+                    surveyService.updateSurvey(request)
+
+                    // then
+                    val actual: Survey = surveyService.findSurvey(
+                        FindSurveyUseCase.Request(createdSurvey.id!!)
+                    ).toDomain()
+
+                    actual.items shouldHaveSize 4
                 }
             }
         }
