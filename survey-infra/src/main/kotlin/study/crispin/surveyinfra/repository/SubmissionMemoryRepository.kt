54eb3ff33bc1767661ebeaca603c9d1fb4b62878
crispin <shoy1415@gmail.com>
2025-02-12 18:47:49 +0900

    :sparkles: Feat: submission save, find

--- survey-infra/src/main/kotlin/study/crispin/surveyinfra/repository/SubmissionMemoryRepository.kt
+++ survey-infra/src/main/kotlin/study/crispin/surveyinfra/repository/SubmissionMemoryRepository.kt
@@ -1,6 +1,32 @@
 package study.crispin.surveyinfra.repository
 
+import java.util.UUID
 import org.springframework.stereotype.Repository
+import study.crispin.surveyinfra.repository.entity.SubmissionEntity
 
 @Repository
-internal class SubmissionMemoryRepository : SubmissionRepository
+internal class SubmissionMemoryRepository : SubmissionRepository {
+    private val storage: MutableMap<UUID, SubmissionEntity> = mutableMapOf()
+
+    override fun save(entity: SubmissionEntity): SubmissionEntity {
+        val savedEntity: SubmissionEntity =
+            when (val id: UUID? = entity.id) {
+                null -> {
+                    entity.run {
+                        val newId: UUID = UUID.randomUUID()
+                        SubmissionEntity
+                            .createWithId(
+                                entity.name,
+                                entity.surveyItemId,
+                                entity.surveyItemVersion,
+                                entity.answers
+                            ).also { storage[newId] = it }
+                    }
+                }
+                else -> entity.update(entity).also { storage[id] = it }
+            }
+        return savedEntity
+    }
+
+    override fun findById(id: UUID): SubmissionEntity? = storage[id]
+}
--- survey-infra/src/main/kotlin/study/crispin/surveyinfra/repository/SubmissionRepository.kt
+++ survey-infra/src/main/kotlin/study/crispin/surveyinfra/repository/SubmissionRepository.kt
@@ -1,3 +1,10 @@
 package study.crispin.surveyinfra.repository
 
-interface SubmissionRepository
+import java.util.UUID
+import study.crispin.surveyinfra.repository.entity.SubmissionEntity
+
+interface SubmissionRepository {
+    fun save(entity: SubmissionEntity): SubmissionEntity
+
+    fun findById(id: UUID): SubmissionEntity?
+}
