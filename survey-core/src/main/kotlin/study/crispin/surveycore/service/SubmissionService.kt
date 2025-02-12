921063b19e521e8275b86c69eeb66a01dde1a295
crispin <h.c.shin.dev09@gmail.com>
2025-02-13 00:07:26 +0900

    :recycle: Refactor: separate service

---
+++ survey-core/src/main/kotlin/study/crispin/surveycore/service/SubmissionService.kt
@@ -0,0 +1,52 @@
+package study.crispin.surveycore.service
+
+import org.springframework.stereotype.Service
+import study.crispin.surveycore.domain.Submission
+import study.crispin.surveycore.domain.SubmissionCollection
+import study.crispin.surveycore.domain.Survey
+import study.crispin.surveycore.extension.toDomain
+import study.crispin.surveycore.extension.toDto
+import study.crispin.surveycore.port.FindSubmissionUseCase
+import study.crispin.surveycore.port.SubmissionUseCase
+import study.crispin.surveycore.port.SubmitSurveyUseCase
+import study.crispin.surveyinfra.port.FindAnswerPort
+import study.crispin.surveyinfra.port.SaveSubmissionPort
+import study.crispin.surveyinfra.port.SubmissionPort
+import study.crispin.surveyinfra.port.SurveyPort
+
+@Service
+class SubmissionService(
+    private val surveyPort: SurveyPort,
+    private val submissionPort: SubmissionPort
+) : SubmissionUseCase {
+    override fun findSubmission(request: FindSubmissionUseCase.Request) {
+        submissionPort.find(
+            FindAnswerPort.Request(
+                request.surveyId,
+                request.version
+            )
+        )
+    }
+
+    override fun submitSurvey(requests: SubmitSurveyUseCase.Requests) {
+        val findedSurvey: Survey =
+            surveyPort
+                .findByIdAndVersion(
+                    requests.surveyId,
+                    requests.version
+                ).toDomain()
+
+        val submissions: List<Submission> = requests.requests.map { it.toDomain() }
+
+        SubmissionCollection(submissions)
+            .submissionValid(findedSurvey.items)
+
+        submissionPort.save(
+            SaveSubmissionPort.Request(
+                requests.surveyId,
+                requests.version,
+                submissions.toDto()
+            )
+        )
+    }
+}
--- survey-core/src/main/kotlin/study/crispin/surveycore/service/SurveyService.kt
+++ survey-core/src/main/kotlin/study/crispin/surveycore/service/SurveyService.kt
@@ -1,28 +1,20 @@
 package study.crispin.surveycore.service
 
 import org.springframework.stereotype.Service
-import study.crispin.surveycore.domain.Submission
-import study.crispin.surveycore.domain.SubmissionCollection
 import study.crispin.surveycore.domain.Survey
 import study.crispin.surveycore.extension.toDomain
 import study.crispin.surveycore.extension.toDto
 import study.crispin.surveycore.port.CreateSurveyUseCase
-import study.crispin.surveycore.port.FindSubmissionUseCase
 import study.crispin.surveycore.port.FindSurveyUseCase
-import study.crispin.surveycore.port.SubmitSurveyUseCase
 import study.crispin.surveycore.port.SurveyUseCase
 import study.crispin.surveycore.port.UpdateSurveyUseCase
 import study.crispin.surveyinfra.adaptor.dto.SurveyDto
-import study.crispin.surveyinfra.port.FindAnswerPort
-import study.crispin.surveyinfra.port.SaveSubmissionPort
 import study.crispin.surveyinfra.port.SaveSurveyPort
-import study.crispin.surveyinfra.port.SubmissionPort
 import study.crispin.surveyinfra.port.SurveyPort
 
 @Service
 internal class SurveyService(
-    private val surveyPort: SurveyPort,
-    private val submissionPort: SubmissionPort
+    private val surveyPort: SurveyPort
 ) : SurveyUseCase {
     override fun createSurvey(request: CreateSurveyUseCase.Request): Survey {
         val saveRequest =
@@ -54,37 +46,6 @@ internal class SurveyService(
         }
     }
 
-    override fun submitSurvey(requests: SubmitSurveyUseCase.Requests) {
-        val findedSurvey: Survey =
-            surveyPort
-                .findByIdAndVersion(
-                    requests.surveyId,
-                    requests.version
-                ).toDomain()
-
-        val submissions: List<Submission> = requests.requests.map { it.toDomain() }
-
-        SubmissionCollection(submissions)
-            .submissionValid(findedSurvey.items)
-
-        submissionPort.save(
-            SaveSubmissionPort.Request(
-                requests.surveyId,
-                requests.version,
-                submissions.toDto()
-            )
-        )
-    }
-
     override fun findSurvey(request: FindSurveyUseCase.Request): SurveyDto =
         surveyPort.findById(request.surveyId)
-
-    override fun findSubmission(request: FindSubmissionUseCase.Request) {
-        submissionPort.find(
-            FindAnswerPort.Request(
-                request.surveyId,
-                request.version
-            )
-        )
-    }
 }
