65c4cc1ea0c8c65e14909ce2b5ecd8d35b3f72ba
crispin <h.c.shin.dev09@gmail.com>
2025-02-12 23:40:49 +0900

    :white_check_mark: Test: submissionAdaptor test

--- survey-infra/src/test/kotlin/study/crispin/surveyinfra/adaptor/SubmissionAdaptorTest.kt
+++ survey-infra/src/test/kotlin/study/crispin/surveyinfra/adaptor/SubmissionAdaptorTest.kt
@@ -1,6 +1,7 @@
 package study.crispin.surveyinfra.adaptor
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldHaveSize
 import java.util.UUID
 import study.crispin.surveyinfra.adaptor.dto.SubmissionDto
 import study.crispin.surveyinfra.port.FindAnswerPort
@@ -8,7 +9,9 @@ import study.crispin.surveyinfra.port.SaveSubmissionPort
 import study.crispin.surveyinfra.port.SubmissionPort
 import study.crispin.surveyinfra.repository.SubmissionMemoryRepository
 import study.crispin.surveyinfra.repository.SubmissionRepository
+import study.crispin.surveyinfra.repository.SurveyItemMemoryRepository
 import study.crispin.surveyinfra.repository.SurveyItemRepository
+import study.crispin.surveyinfra.repository.SurveyMemoryRepository
 import study.crispin.surveyinfra.repository.SurveyRepository
 import study.crispin.surveyinfra.repository.entity.FormEmbeddable
 import study.crispin.surveyinfra.repository.entity.FormType
@@ -27,6 +30,8 @@ class SubmissionAdaptorTest :
             val surveyId = UUID(0L, 0L)
             val surveyItemId = UUID(-1L, -1L)
             submissionRepository = SubmissionMemoryRepository()
+            surveyRepository = SurveyMemoryRepository()
+            surveyItemRepository = SurveyItemMemoryRepository()
             submissionAdaptor =
                 SubmissionAdaptor(
                     submissionRepository
@@ -42,37 +47,33 @@ class SubmissionAdaptorTest :
 
             surveyItemRepository.saveAll(
                 listOf(
-                    SurveyItemEntity.createWithId(
+                    SurveyItemEntity(
                         surveyId,
                         "이름",
                         "성함을 입력해주세요.",
                         FormEmbeddable(FormType.SHORT_INPUT),
-                        true,
-                        surveyItemId
+                        true
                     ),
-                    SurveyItemEntity.createWithId(
+                    SurveyItemEntity(
                         surveyId,
                         "나이",
                         "나이를 입력해주세요.",
                         FormEmbeddable(FormType.SHORT_INPUT),
-                        true,
-                        surveyItemId
+                        true
                     ),
-                    SurveyItemEntity.createWithId(
+                    SurveyItemEntity(
                         surveyId,
                         "관심사",
                         "관심 있는 분야를 모두 선택해주세요.",
                         FormEmbeddable(FormType.MULTI_SELECT, listOf("스포츠", "음악", "돈")),
-                        true,
-                        surveyItemId
+                        true
                     ),
-                    SurveyItemEntity.createWithId(
+                    SurveyItemEntity(
                         surveyId,
                         "성별",
                         "관심 있는 분야를 모두 선택해주세요.",
                         FormEmbeddable(FormType.SINGLE_SELECT, listOf("남", "여", "기타")),
-                        true,
-                        surveyItemId
+                        true
                     )
                 )
             )
@@ -87,10 +88,9 @@ class SubmissionAdaptorTest :
                     it("응답을 저장 할 수 있어야 한다.") {
                         // given
                         val surveyId = UUID(0L, 0L)
-                        val surveyItemId = UUID(-1L, -1L)
                         val request =
                             SaveSubmissionPort.Request(
-                                surveyItemId,
+                                surveyId,
                                 1,
                                 listOf(
                                     SubmissionDto(
@@ -115,12 +115,62 @@ class SubmissionAdaptorTest :
                         submissionAdaptor.save(request)
 
                         // then
-                        submissionAdaptor.find(
-                            FindAnswerPort.Request(
-                                surveyId,
-                                1
+                        val submissionDtos: List<SubmissionDto> =
+                            submissionAdaptor.find(
+                                FindAnswerPort.Request(
+                                    surveyId,
+                                    1
+                                )
                             )
-                        )
+
+                        submissionDtos shouldHaveSize 4
+                    }
+                }
+            }
+
+            describe("응답 조회 테스트") {
+
+                context("정상적인 설문조사 아이디와 응답 버전을 통한 조회 요청일 때") {
+
+                    it("응답을 조회 할 수 있어야 한다.") {
+                        // given
+                        val surveyId = UUID(0L, 0L)
+                        val request =
+                            SaveSubmissionPort.Request(
+                                surveyId,
+                                1,
+                                listOf(
+                                    SubmissionDto(
+                                        "이름",
+                                        listOf("응답봇1")
+                                    ),
+                                    SubmissionDto(
+                                        "나이",
+                                        listOf("10")
+                                    ),
+                                    SubmissionDto(
+                                        "관심사",
+                                        listOf("스포츠", "음악")
+                                    ),
+                                    SubmissionDto(
+                                        "성별",
+                                        listOf("남")
+                                    )
+                                )
+                            )
+                        submissionAdaptor.save(request)
+
+                        // when
+                        val result: List<SubmissionDto> =
+                            submissionAdaptor.find(
+                                FindAnswerPort.Request(
+                                    surveyId,
+                                    1
+                                )
+                            )
+
+                        // then
+                        result shouldHaveSize 4
                     }
                 }
             }
