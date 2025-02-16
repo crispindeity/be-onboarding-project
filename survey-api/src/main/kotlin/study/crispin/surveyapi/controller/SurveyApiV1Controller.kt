8f5256b27976a19bbbbfaf4e599cfa5ae026b7c4
crispin <h.c.shin.dev09@gmail.com>
2025-02-17 00:24:58 +0900

    :sparkles: Feat: survey api controller

---
+++ survey-api/src/main/kotlin/study/crispin/surveyapi/controller/SurveyApiV1Controller.kt
@@ -0,0 +1,30 @@
+package study.crispin.surveyapi.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import study.crispin.surveyapi.controller.dto.request.CreateSurveyRequest
+import study.crispin.surveyapi.controller.dto.response.Response
+import study.crispin.surveyapi.extensions.toDomain
+import study.crispin.surveycore.port.CreateSurveyUseCase
+
+@RestController
+@RequestMapping("/api/survey")
+class SurveyApiV1Controller(
+    private val createSurveyUseCase: CreateSurveyUseCase
+) {
+    @GetMapping(produces = ["application/json", "application/vnd.crispin.survey-v1+json"])
+    fun createSurvey(
+        @RequestBody request: CreateSurveyRequest
+    ): Response<Nothing> {
+        createSurveyUseCase.createSurvey(
+            CreateSurveyUseCase.Request(
+                request.name,
+                request.description,
+                request.items.toDomain()
+            )
+        )
+        return Response.success()
+    }
+}
