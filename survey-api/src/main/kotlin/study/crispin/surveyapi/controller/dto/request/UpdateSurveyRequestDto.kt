cb027a3771b33bb202d9f5e68a112a588ce7ee8c
crispin <h.c.shin.dev09@gmail.com>
2025-02-26 00:14:11 +0900

    :bug: Fix: add serializable annotation

--- survey-api/src/main/kotlin/study/crispin/surveyapi/controller/dto/request/UpdateSurveyRequestDto.kt
+++ survey-api/src/main/kotlin/study/crispin/surveyapi/controller/dto/request/UpdateSurveyRequestDto.kt
@@ -1,8 +1,12 @@
 package study.crispin.surveyapi.controller.dto.request
 
 import java.util.UUID
+import kotlinx.serialization.Serializable
+import study.crispin.surveyapi.config.UUIDSerializer
 
+@Serializable
 data class UpdateSurveyRequestDto(
+    @Serializable(with = UUIDSerializer::class)
     val id: UUID,
     val name: String,
     val description: String,
